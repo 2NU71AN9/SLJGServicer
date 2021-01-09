b4c92f780707e4314e82bd3c727469dd06b61162
2UN7 <1491859758@qq.com>
2021-01-09 14:36:06 +0800

    极光推送

--- SLJGServicer/AppDelegate.swift
+++ SLJGServicer/AppDelegate.swift
@@ -12,14 +12,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    let appkey = "b73e02f4f8fdc7b3f9cd665f"
-    let appSecret = "2de8d4d474b809af7724c72b"
+    let appkey = "3f630eb745c69f0dcf56ee54"
+    let appSecret = "eb89a20364a362aa9781fad1"
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         _ = SLJGServicer.shared
             .registAnalytics(appkey)
             .registShare(appKey: appkey, universalLink: "", wechatAppId: "", wechatAppSecret: "")
+            .registPush(appKey: appkey, launchOptions: launchOptions)
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        SLJGServicer.shared.registDeviceToken(deviceToken)
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
--- SLJGServicer/SLJGServicer-Bridging-Header.h
+++ SLJGServicer/SLJGServicer-Bridging-Header.h
@@ -5,6 +5,11 @@
 #import <UIKit/UIKit.h>
 #import <JANALYTICSService.h>
 #import <JSHAREService.h>
+#import <JPUSHService.h>
+
+//#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+//#import <UserNotifications/UserNotifications.h>
+//#endif
 
 // 如果需要使用idfa功能所需要引入的头文件（可选）
 //#import <AdSupport/AdSupport.h>
