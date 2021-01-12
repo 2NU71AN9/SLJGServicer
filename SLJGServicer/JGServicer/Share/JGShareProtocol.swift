16e41d788ddeb4a1a2b33f2527bdc0d77d3c818b
2UN7 <1491859758@qq.com>
2021-01-12 11:28:59 +0800

    demo

--- SLJGServicer/JGServicer/Share/JGShareProtocol.swift
+++ SLJGServicer/JGServicer/Share/JGShareProtocol.swift
@@ -14,7 +14,13 @@ public protocol JGShareProtocol {
     /// 获取用户在第三方平台的用户 ID、头像等资料完成账号体系的构建
     func getSocialUserInfo(platform: JSHAREPlatform, complete: @escaping ((JSHARESocialUserInfo?) -> Void))
     /// 分享
-    func share(_ message: JSHAREMessage, complete: ((Bool) -> Void)?)
+    func shareText(_ text: String?, complete: ((Bool) -> Void)?)
+    func shareImage(_ image: Any?, complete: ((Bool) -> Void)?)
+    func shareUrl(_ url: String, title: String?, text: String?, image: Any?, complete: ((Bool) -> Void)?)
+    func shareVideo(_ url: String, title: String?, text: String?, image: Any?, complete: ((Bool) -> Void)?)
+    func shareAudio(_ url: String, title: String?, text: String?, image: Any?, complete: ((Bool) -> Void)?)
+    // type 0正式版，1开发版，2体验版。默认0，正式版
+    func shareMiniApp(title: String, text: String?, url: String, userName: String, path: String, type: Int?, image: Any?, complete: ((Bool) -> Void)?)
     /// 可以添加各种分享内容的方法
 }
 
@@ -44,7 +50,63 @@ public extension JGShareProtocol where Self: SLJGServicer {
         }
     }
     
-    func share(_ message: JSHAREMessage, complete: ((Bool) -> Void)?) {
+    func shareText(_ text: String?, complete: ((Bool) -> Void)?) {
+        guard let text = text else { return }
+        let message = JSHAREMessage()
+        message.text = text
+        message.mediaType = .text
+        share(message, complete: complete)
+    }
+    func shareImage(_ image: Any?, complete: ((Bool) -> Void)?) {
+        guard let image = image2Data(image) else { return }
+        let message = JSHAREMessage()
+        message.image = image
+        message.mediaType = .image
+        share(message, complete: complete)
+    }
+    func shareUrl(_ url: String, title: String?, text: String?, image: Any?, complete: ((Bool) -> Void)?) {
+        let message = JSHAREMessage()
+        message.url = url
+        message.title = title
+        message.text = text
+        message.image = image2Data(image)
+        message.mediaType = .link
+        share(message, complete: complete)
+    }
+    func shareVideo(_ url: String, title: String?, text: String?, image: Any?, complete: ((Bool) -> Void)?) {
+        let message = JSHAREMessage()
+        message.url = url
+        message.title = title
+        message.text = text
+        message.image = image2Data(image)
+        message.mediaType = .video
+        share(message, complete: complete)
+    }
+    func shareAudio(_ url: String, title: String?, text: String?, image: Any?, complete: ((Bool) -> Void)?) {
+        let message = JSHAREMessage()
+        message.url = url
+        message.title = title
+        message.text = text
+        message.image = image2Data(image)
+        message.mediaType = .audio
+        share(message, complete: complete)
+    }
+    func shareMiniApp(title: String, text: String?, url: String, userName: String, path: String, type: Int?, image: Any?, complete: ((Bool) -> Void)?) {
+        guard let image = image2Data(image) else { return }
+        let message = JSHAREMessage()
+        message.mediaType = .miniProgram
+        message.title = title
+        message.text = text
+        message.url = url
+        message.userName = userName
+        message.path = path
+        message.miniProgramType = Int32(type ?? 0)
+        message.image = image
+        message.platform = .wechatSession
+        share(message, complete: complete)
+    }
+    
+    private func share(_ message: JSHAREMessage, complete: ((Bool) -> Void)?) {
         
         func share() {
             JSHAREService.share(message) { (state, error) in
@@ -61,5 +123,17 @@ public extension JGShareProtocol where Self: SLJGServicer {
                 share()
             }.show()
         }
+    }
+    
+    private func image2Data(_ image: Any?) -> Data? {
+        guard let image = image else { return nil }
+        if let image = image as? UIImage {
+            return image.pngData() ?? image.jpegData(compressionQuality: 1.0)
+        } else if let image = image as? String, let imageUrl = URL(string: image) {
+            return try? Data(contentsOf: imageUrl)
+        } else if let image = image as? URL {
+            return try? Data(contentsOf: image)
+        }
+        return nil
     }
 }
--- SLJGServicer/ViewController.swift
+++ SLJGServicer/ViewController.swift
@@ -7,20 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesEnded(touches, with: event)
-        let message = JSHAREMessage()
-        message.text = "123123123123123"
-        message.mediaType = .text
-        SLJGServicer.shared.share(message) { (isSuccess) in
-            print("分享结果==>(isSuccess)")
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            SLJGServicer.shared.shareText("这是极光分享内容") { (isSuccess) in
+                print("分享结果==>\(isSuccess)")
+            }
+        case (0, 1):
+            SLJGServicer.shared.shareImage("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1091405991,859863778&fm=26&gp=0.jpg") { (isSuccess) in
+                print("分享结果==>\(isSuccess)")
+            }
+        case (0, 2):
+            SLJGServicer.shared.shareUrl("www.baidu.com", title: "这是链接", text: "这是内容这是内容这是内容这是内容这是内容这是内容", image: UIImage(named: "wechatTimeLine50")) { (isSuccess) in
+                print("分享结果==>\(isSuccess)")
+            }
+        case (0, 3):
+            SLJGServicer.shared.shareVideo("http://v.youku.com/v_show/id_XOTQwMDE1ODAw.html?from=s1.8-1-1.2&spm=a2h0k.8191407.0.0", title: "这是视频", text: "这是内容这是内容这是内容这是内容这是内容这是内容", image: UIImage(named: "wechatTimeLine50")) { (isSuccess) in
+                print("分享结果==>\(isSuccess)")
+            }
+        case (0, 4):
+            SLJGServicer.shared.shareAudio("https://y.qq.com/n/yqq/song/003RCA7t0y6du5.html", title: "这是音频", text: "这是内容这是内容这是内容这是内容这是内容这是内容", image: UIImage(named: "wechatTimeLine50")) { (isSuccess) in
+                print("分享结果==>\(isSuccess)")
+            }
+        case (0, 5):
+            SLJGServicer.shared.shareMiniApp(title: "这是小程序", text: "小程序描述", url: "www.jiguang.cn", userName: "gh_d43f693ca31f", path: "pages/page", type: nil, image: UIImage(named: "wechatTimeLine50")) { (isSuccess) in
+                print("分享结果==>\(isSuccess)")
+            }
+        default:
+            break
         }
     }
 }
